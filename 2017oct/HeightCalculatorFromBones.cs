5256e307dac183e913d511a5d208db45be39bdc6
Wesley Cox <cwesnow2000@yahoo.com>
2017-10-18 21:12:41 -0500

    Update HeightCalculatorFromBones.cs
    Minor Refactor

--- 2017oct/HeightCalculatorFromBones.cs
+++ 2017oct/HeightCalculatorFromBones.cs
@@ -2,7 +2,7 @@
 // Author: GitGub on Reddit
 // Link: https://www.reddit.com/r/learnprogramming/comments/77a49n/c_help_refactoring_an_ugly_method_with_lots_of_if/
 
-// Refactored using .NET Fiddle - https://dotnetfiddle.net/F8UcJW
+// Refactored using .NET Fiddle - https://dotnetfiddle.net/YiG9XQ
 using System;
 					
 public class Program
@@ -10,11 +10,10 @@ public class Program
 	static Random random = new Random();
 	
 	enum Bones { Femur, Tibia, Humerus, Radius }
-	enum Gender { Male, Female}
+	enum Gender { Male, Female }
 	
 	public static void Main()
 	{
-		
 		for(int x = 0; x < 5; x++) {
 			
 			// Get Randome Values
@@ -32,27 +31,32 @@ public class Program
 	private static float calculateHeight(Bones bone, float boneLength, Gender gender, int age)
     {
         float height = 0;
-
+		float A = age * 0.06f;
+		
 		switch(bone) {
 			
 			case Bones.Femur:
 				height = (gender == Gender.Male) ?
-					69.089f + (2.238f * boneLength) - age * 0.06f : 61.412f + (2.317f * boneLength) - age * 0.06f;
+					69.089f + (2.238f * boneLength) - A : // Male
+					61.412f + (2.317f * boneLength) - A; // Female
 				break;
 			
 			case Bones.Tibia:
 				height = (gender == Gender.Male) ?
-					81.688f + (2.392f * boneLength) - age * 0.06f : 72.572f + (2.533f * boneLength) - age * 0.06f;
+					81.688f + (2.392f * boneLength) - A : // Male
+					72.572f + (2.533f * boneLength) - A; // Female
 				break;
 			
 			case Bones.Humerus:
 				height = (gender == Gender.Male) ?
-					73.570f + (2.970f * boneLength) - age * 0.06f : 64.977f + (3.144f * boneLength) - age * 0.06f;
+					73.570f + (2.970f * boneLength) - A : // Male
+					64.977f + (3.144f * boneLength) - A; // Female
 				break;
 			
 			case Bones.Radius:
 				height = (gender == Gender.Male) ?
-					80.405f + (3.650f * boneLength) - age * 0.06f : 73.502f + (3.876f * boneLength) - age * 0.06f;
+					80.405f + (3.650f * boneLength) - A : // Male
+					73.502f + (3.876f * boneLength) - A; // Female
 				break;
 			
 			default: break;
@@ -61,7 +65,8 @@ public class Program
     }
 	
 	private static float randomLength() { return (float)random.NextDouble()*3; }
-	private static Bones randomBone() { return (Bones)random.Next(1,4); }
-	private static Gender randomGender() { return (Gender)random.Next(0,2); } // 1,2 & 0,1 always gave the same gender
-	private static int randomAge() { return random.Next(16, 99); }	
+	private static Bones randomBone() { return (Bones)random.Next(0,3); }
+	private static Gender randomGender() { return (Gender)random.Next(0,2); } // 0,2 works better than 1,2 or 0,1. Unknown reasons
+	private static int randomAge() { return random.Next(16, 99); }
+	
 }
