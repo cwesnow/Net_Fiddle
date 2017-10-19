54987b86b15bd09e929a5a99b283101d93585036
Wesley Cox <cwesnow2000@yahoo.com>
2017-10-18 20:22:02 -0500

    Update HeightCalculatorFromBones.cs

--- 2017oct/HeightCalculatorFromBones.cs
+++ 2017oct/HeightCalculatorFromBones.cs
@@ -1,48 +1,67 @@
+// Original Source Information
 // Author: GitGub on Reddit
 // Link: https://www.reddit.com/r/learnprogramming/comments/77a49n/c_help_refactoring_an_ugly_method_with_lots_of_if/
 
-    //Calculate height from boneLength, bone, gender, and age
-    private float CalculateHeight(float boneLength, string boneType, string gender, int age)
+// Refactored using .NET Fiddle - https://dotnetfiddle.net/F8UcJW
+using System;
+					
+public class Program
+{
+	static Random random = new Random();
+	
+	enum Bones { Femur, Tibia, Humerus, Radius }
+	enum Gender { Male = 1, Female = 2 }
+	
+	public static void Main()
+	{
+		
+		for(int x = 0; x < 5; x++) {
+			
+			// Get Randome Values
+			float length = randomLength();
+			Bones bone = randomBone();
+			Gender gender = randomGender();
+			int age = randomAge();
+			
+		// Result
+			Console.WriteLine("\nGender: {0}\nAge: {1}\nBone: {2}\nLength: {3}\nHeight: {4}",
+						  gender, age, bone, length, calculateHeight(bone, length, gender, age));
+		}
+	}
+	
+	private static float calculateHeight(Bones bone, float boneLength, Gender gender, int age)
     {
         float height = 0;
 
-        if (gender == "Male")
-        {
-            if (boneType == "Femur")
-            {
-                height = 69.089f + (2.238f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Tibia")
-            {
-                height = 81.688f + (2.392f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Humerus")
-            {
-                height = 73.570f + (2.970f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Radius")
-            {
-                height = 80.405f + (3.650f * boneLength) - age * 0.06f;
-            }
-        }
-        else if (gender == "Female")
-        {
-            if (boneType == "Femur")
-            {
-                height = 61.412f + (2.317f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Tibia")
-            {
-                height = 72.572f + (2.533f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Humerus")
-            {
-                height = 64.977f + (3.144f * boneLength) - age * 0.06f;
-            }
-            else if (boneType == "Radius")
-            {
-                height = 73.502f + (3.876f * boneLength) - age * 0.06f;
-            }
-        }
-        return height;
+		switch(bone) {
+			
+			case Bones.Femur:
+				height = (gender == Gender.Male) ?
+					69.089f + (2.238f * boneLength) - age * 0.06f : 61.412f + (2.317f * boneLength) - age * 0.06f;
+				break;
+			
+			case Bones.Tibia:
+				height = (gender == Gender.Male) ?
+					81.688f + (2.392f * boneLength) - age * 0.06f : 72.572f + (2.533f * boneLength) - age * 0.06f;
+				break;
+			
+			case Bones.Humerus:
+				height = (gender == Gender.Male) ?
+					height = 73.570f + (2.970f * boneLength) - age * 0.06f : 64.977f + (3.144f * boneLength) - age * 0.06f;
+				break;
+			
+			case Bones.Radius:
+				height = (gender == Gender.Male) ?
+					80.405f + (3.650f * boneLength) - age * 0.06f : 73.502f + (3.876f * boneLength) - age * 0.06f;
+				break;
+			
+			default: break;
+		}
+		return height;
     }
+	
+	private static float randomLength() { return (float)random.NextDouble()*3; }
+	private static Bones randomBone() { return (Bones)random.Next(1,4); }
+	private static Gender randomGender() { return (Gender)random.Next(1,2); }
+	private static int randomAge() { return random.Next(16, 99); }	
+}
